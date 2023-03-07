19e2a3613f99de650f00ba29e3e981823f0d77a0
Ruslan Yarullin <ryarullin553@gmail.com>
2023-03-07 20:17:58 +0300

    Makemigrations

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField(upload_to='images/%Y/%m/%d/')
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def test(request):
+    return HttpResponse("Test")
+
+
+def categories(request, catid):
+    return HttpResponse(catid)
